e904f6f7c4dbc08f0ad8ddb912673ecad00781dc
vamoosev <vamoosev@gmail.com>
2023-06-18 16:12:37 +0300

    joojoo

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,44 @@
-use pcap::{Device,Packet};
+use pcap::{Device, Packet, Capture};
 use serde_json;
 use reqwest;
 use chrono;
 #[tokio::main]
 async fn main() {
+    // set device to monitor mode (if not already)
+
+    let device_name = "wlan0";
+    let output = std::process::Command::new("iw")
+        .arg("dev")
+        .arg(device_name)
+        .arg("set")
+        .arg("monitor")
+        .output()
+        .expect("failed to execute process");
+    if output.status.success() {
+        println!("Device {} is now in monitor mode.", device_name);
+    } else {
+        println!("Failed to set monitor mode on device {}", device_name);
+    }
     //get device
     //open device
     let  mut cap = Device::lookup().unwrap().unwrap().open().unwrap();
-    //crete vector to store packets
+    
+
+    // set filter for wifi scan packets
+    cap.filter("type mgt subtype probe-req", true).unwrap();
     let mut packets: Vec<Packet> = Vec::new();
     // capture packets while time is less than 7pm
     // BUG: cannot borrow cap as mutable more than once at a time
     // how to fix?
-    while let Ok(packet) = cap.next_packet() {
-        let time = chrono::Local::now();
-         if chrono::NaiveTime::from_hms(19, 0, 0) > time.time(){
-            packets.push(packet.clone());
-        } else {
-            break;
-        }
+    // also check if built with debug if debug then stop after 10 packets
+    while chrono::Local::now().time() < chrono::NaiveTime::from_hms(15, 55, 0) {
+        let packet = cap.next_packet().unwrap();
+        println!("packet: {:?}", packet);
     }
-
 
     // convert pacekts to mac addresses
     // convert mac addresses to json
     let  mut macs: Vec<String> = Vec::new();
-    for packet in packets {
-        let mac = format!("{:x}:{:x}:{:x}:{:x}:{:x}:{:x}", packet.data[6], packet.data[7], packet.data[8], packet.data[9], packet.data[10], packet.data[11]);
-        macs.push(mac);
-    }
-
-
 
 
     let json = serde_json::to_string(&macs).unwrap();
